596fbf460c0d202489f5a7745a28363e6bd20e9c
optout <13562139+optout21@users.noreply.github.com>
2026-05-17 11:05:00 +0200

    bench: Add benchmark for  PresaltedXorHasher

--- src/bench/crypto_hash.cpp
+++ src/bench/crypto_hash.cpp
@@ -15,6 +15,7 @@
 #include <span.h>
 #include <tinyformat.h>
 #include <uint256.h>
+#include <util/hasher.h>
 
 #include <cstdint>
 #include <vector>
@@ -203,6 +204,36 @@ static void SipHash_32b(benchmark::Bench& bench)
     });
 }
 
+static void XorHasher6x8_1_36b(benchmark::Bench& bench)
+{
+    FastRandomContext rng{/*fDeterministic=*/true};
+    PresaltedXorHasher6x8_1 presalted_xor_hasher(rng.rand64());
+    auto val{rng.rand256()};
+    uint32_t extra{rng.rand32()};
+    auto i{0U};
+    bench.run([&] {
+        ankerl::nanobench::doNotOptimizeAway(presalted_xor_hasher(val, extra));
+        ++i;
+        val.data()[i % uint256::size()] ^= i & 0xFF;
+        extra += i;
+    });
+}
+
+static void XorHasher6x8_2_36b(benchmark::Bench& bench)
+{
+    FastRandomContext rng{/*fDeterministic=*/true};
+    PresaltedXorHasher6x8_2 presalted_xor_hasher(rng.rand64(), rng.rand64(), rng.rand64(), rng.rand64());
+    auto val{rng.rand256()};
+    uint32_t extra{rng.rand32()};
+    auto i{0U};
+    bench.run([&] {
+        ankerl::nanobench::doNotOptimizeAway(presalted_xor_hasher(val, extra));
+        ++i;
+        val.data()[i % uint256::size()] ^= i & 0xFF;
+        extra += i;
+    });
+}
+
 static void MuHash(benchmark::Bench& bench)
 {
     MuHash3072 acc;
@@ -274,6 +305,8 @@ BENCHMARK(SHA256_32b_SSE4);
 BENCHMARK(SHA256_32b_AVX2);
 BENCHMARK(SHA256_32b_SHANI);
 BENCHMARK(SipHash_32b);
+BENCHMARK(XorHasher6x8_1_36b);
+BENCHMARK(XorHasher6x8_2_36b);
 BENCHMARK(SHA256D64_1024_STANDARD);
 BENCHMARK(SHA256D64_1024_SSE4);
 BENCHMARK(SHA256D64_1024_AVX2);
